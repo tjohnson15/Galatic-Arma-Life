39bbb2c43a7dbf194b08109e9875acdb25bcbea8
Tyler Ellis <32233288+tjohnson15@users.noreply.github.com>
2023-04-11 01:50:41 -0400

    Updated Fuel Price

--- config/Config_Master.hpp
+++ config/Config_Master.hpp
@@ -81,7 +81,7 @@ class Life_Settings {
 
     /* Fuel System Configurations */
     pump_service = false; //Allow users to use pump service on the map. Default = false
-    fuel_cost = 80; //Cost of fuel per liter at fuel stations (if not defined for the vehicle already).
+    fuel_cost = 350; //Cost of fuel per liter at fuel stations (if not defined for the vehicle already).
     service_chopper = 1000; //Cost to service chopper at chopper service station(Repair/Refuel).
     fuelCan_refuel = 250; //Cost to refuel an empty fuel canister at the fuel station pumps. (Be wary of your buy/sell prices on fuel cans to prevent exploits...)
 
